6280210f3c107f065bb1dee9119f67bd4e1a90fb
priyya0208 <54065326+priyya0208@users.noreply.github.com>
2022-12-10 20:00:29 +0000

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component, VERSION } from '@angular/core';
+import { interval, map, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  source$ = interval(1000).pipe(take(2));
+  combineAllObj = this.source$.pipe(
+    map((val) =>
+      interval(1000).pipe(
+        map((i) => `Result (${val}): ${i}`),
+        take(5)
+      )
+    )
+  );
 }
